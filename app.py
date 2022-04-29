8b11d3f1447c73326284151a1fd4dd15e1df6742
capibur <nenascheff.an@yandex.com>
2022-04-29 13:50:56 +0300

    procfile

--- app.py
+++ app.py
@@ -232,4 +232,4 @@ api.add_resource(rec_api.ToPlaylist, "/pl_api/<string:choose_playlist>",
                  "/pl_api/<string:choose_playlist>/<string:track_id>")
 api.add_resource(rec_api.TrackInPlayList, "/track_pl/<int:playlist_id>", "/track_pl/<int:playlist_id>/<int:track_id>")
 port = int(os.environ.get('PORT', 5000))
-app.run(host='127.0.0.1', port=port, debug=True)
+app.run(host='0.0.0.0', port=port, debug=True)
